f806f24088df697d27bbef3dd71fc51b7e02fb40
HeitorNOC <heiitornoc@gmail.com>
2023-07-17 19:02:13 -0300

    user entity

---
+++ back-api/src/domain/entity/User.ts
@@ -0,0 +1,19 @@
+class UserEntity {
+    public ID: number | null
+    public name: string
+    public email: string
+    public password: string
+    public birthday: Date
+    public createdAt: Date
+    public updatedAt: Date
+
+    constructor(ID: number | null, name: string, email: string, password: string, birthday: Date, createdAt: Date, updatedAt: Date) {
+        this.ID = ID
+        this.name = name
+        this.email = email
+        this.password = password
+        this.birthday = birthday
+        this.createdAt = createdAt
+        this.updatedAt = updatedAt
+    }
+}
