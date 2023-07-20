8d56cd6a8153cccfea66120001c994a19cea4d7d
Heitor <heiitornoc@gmail.com>
2023-07-20 18:13:51 -0300

    folder fix

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -1,12 +1,23 @@
-import { ObjectId } from "mongoose";
+class UserEntity {
+    public ID: number | null
+    public name: string
+    public email: string
+    public password: string
+    public birthday: Date
+    public created_at: Date
+    public updated_at: Date
 
-export interface Entity {
-  _id:  ObjectId;
+    constructor(ID: number | null, name: string, email: string, password: string, birthday: Date, created_at: Date, updated_at: Date) {
+        this.ID = ID
+        this.name = name
+        this.email = email
+        this.password = password
+        this.birthday = birthday
+        this.created_at = created_at
+        this.updated_at = updated_at
+    }
 }
 
-export interface User extends Entity {
-  name: string;
-  email: string;
+export {
+    UserEntity
 }
-
-export type CreateUserInput = Omit<User, "_id">;
--- src/infrastructure/provider/repository/UserRepository.ts
+++ src/infrastructure/provider/repository/UserRepository.ts
@@ -1,53 +1,44 @@
-import mongoose from 'mongoose';
-import { CreateUserInput, User } from '../../../domain/entity/User';
-import { UserModel, UserDocument } from '../../internal/UserModel';
+import { UserEntity } from "../../../domain/entity/User";
+import { CreateUserUseCaseRepositoryInterface, DeleteUserUseCaseRepositoryInterface, GetAllUsersUseCaseRepositoryInterface, GetUserByIdUseCaseRepositoryInterface, UpdateUserUseCaseRepositoryInterface } from "../../../domain/usecase/repository/userRepoInterface";
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../../internal/database/postgresql/user";
 
-type ObjectId = mongoose.Types.ObjectId
+class GetAllUsersUseCaseRepository implements GetAllUsersUseCaseRepositoryInterface {
+    async getAllUsers(): Promise<(UserEntity[] | null)> {
+        return await getAllUsers()
+    }   
+}
 
-export class UserRepository {
-  async getAllUsers(): Promise<User[]> {
-    const users: UserDocument[] = await UserModel.find().exec();
-    return users.map((user) => this.mapToUser(user));
-  }
-
-  async getUserById(id: ObjectId): Promise<User | null> {
-    const user: UserDocument | null = await UserModel.findById(id).exec();
-    return user ? this.mapToUser(user) : null;
-  }
-
-  async createUser(user: CreateUserInput): Promise<User> {
-    const createdUser: UserDocument = await UserModel.create(user);
-    return this.mapToUser(createdUser);
-  }
-
-  async updateUser(id: ObjectId, user: CreateUserInput): Promise<User | null> {
-    const userToUpdate: UserDocument | null = await UserModel.findOneAndUpdate({_id: id },  {
-      name: user.name,
-      email: user.email
-     },).exec()
-
-    const userUpdated = await this.getUserById(id)
-
-    if (userUpdated == null) {
-      return null
-    } 
+class GetUserByIdUseCaseRepository implements GetUserByIdUseCaseRepositoryInterface {
+    async getUserById(id: number): Promise<UserEntity | null> {
+        return await getUserById(id)
+    }
     
-    return userToUpdate != null ? this.mapToUser(userUpdated) : null;
-  }
+}
 
-  async deleteUser(id: ObjectId): Promise<User | null> {
-    const userToDelete: UserDocument | null = await UserModel.findOneAndDelete({ _id: id }).exec()
-
-    return userToDelete != null ? this.mapToUser(userToDelete): null
-
+class CreateUserUseCaseRepository implements CreateUserUseCaseRepositoryInterface {
+    async createUser(name: string, email: string, password: string, birthday: Date): Promise<UserEntity | null> {
+        return await createUser(name, email, password, birthday)
+    }
     
-  }
+}
 
-  private mapToUser(userModel: UserDocument | User): User {
-    return {
-      _id: userModel._id,
-      name: userModel.name,
-      email: userModel.email,
-    };
-  }
+class UpdateUserUseCaseRepository implements UpdateUserUseCaseRepositoryInterface {
+    async updateUser(id: number, name: string, email: string, password: string, birthday: Date): Promise<UserEntity | null> {
+        return await updateUser(id, name, email, password, birthday)
+    }
+    
+}
+
+class DeleteUserUseCaseRepository implements DeleteUserUseCaseRepositoryInterface {
+    async deleteUser(id: number): Promise<UserEntity | null> {
+        return await deleteUser(id)
+    }
+}
+
+export {
+    GetAllUsersUseCaseRepository,
+    GetUserByIdUseCaseRepository,
+    CreateUserUseCaseRepository,
+    UpdateUserUseCaseRepository,
+    DeleteUserUseCaseRepository
 }
